70a162e45fe4f96f5a4632c9b5985839940dbc08
Vishal Kumar Singh <singhvishal0847@gmail.com>
2022-04-01 22:16:57 +0530

    Initial Commit

---
+++ src/Recursion2.java
@@ -0,0 +1,12 @@
+public class Recursion2 {
+    public static int fun(int n){
+        if (n == 1)
+            return 0;
+        else
+            return (1 + fun(n/2));
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fun(16));
+    }
+}
