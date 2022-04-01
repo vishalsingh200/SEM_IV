81d15089b325034f967c3395331205b2243b6edf
Vishal Kumar Singh <singhvishal0847@gmail.com>
2022-04-01 22:35:18 +0530

    Initial Commit

---
+++ src/Recursion3.java
@@ -0,0 +1,12 @@
+public class Recursion3 {
+    public static void fun(int n){
+        if (n == 0)
+            return;
+        fun(n/2);
+        System.out.println(n%2);
+    }
+
+    public static void main(String[] args) {
+        fun(7);
+    }
+}
